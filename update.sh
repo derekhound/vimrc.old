13b146daf910d902f7adb69fd28ffb6992d38159
derekchen <derekhound@gmail.com>
2015-03-09 21:35:11 +0800

    remove filetypes.vim

--- update.sh
+++ update.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-rm -rf vimrc
-
-cat vimrcs/basic.vim     >> vimrc
-cat vimrcs/filetypes.vim >> vimrc
-cat vimrcs/plugins.vim   >> vimrc
+cat vimrcs/basic.vim > vimrc
+cat vimrcs/plugins.vim >> vimrc
